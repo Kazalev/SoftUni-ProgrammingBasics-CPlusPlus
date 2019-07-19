f7e8a87a242e98d9c9ba499500b353ebbb564fb6
Kazalev <kristian.kazalev@abv.bg>
2019-07-19 20:54:32 +0300

    Task

---
+++ Programming Basics with C++ - September 2018/03. Lesson/01. Excellent Result/Excellent Result.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    double grade;
+    cin >> grade;
+
+    if (grade >= 5.50){
+    cout << "Excellent!" << endl;
+    }
+
+    return 0;
+}
+
