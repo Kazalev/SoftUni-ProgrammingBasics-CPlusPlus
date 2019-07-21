57488594c06a212f97ab6e4444b79a29ff53111a
Kazalev <kristian.kazalev@abv.bg>
2019-07-21 10:49:02 +0300

    Task

---
+++ Programming Basics with C++ - September 2018/03. Lesson/06. Number 100...200/Number 100...200.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    int number;
+    cin >> number;
+
+    if (number < 100){
+        cout << "Less than 100" << endl;
+    }
+    else if (number > 200){
+        cout << "Greater than 200" << endl;
+    }
+    else
+        cout << "Between 100 and 200" << endl;
+
+    return 0;
+}
