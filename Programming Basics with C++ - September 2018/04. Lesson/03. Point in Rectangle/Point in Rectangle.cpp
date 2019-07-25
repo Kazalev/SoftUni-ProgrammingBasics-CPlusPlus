f26c099b5ce9f60e8a3ad73ff6dcf440fbcff5bc
Kazalev <kristian.kazalev@abv.bg>
2019-07-25 13:04:33 +0300

    Task

---
+++ Programming Basics with C++ - September 2018/04. Lesson/03. Point in Rectangle/Point in Rectangle.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    double x1, y1, x2, y2, x, y;
+    cin >> x1 >> y1 >> x2 >> y2 >> x >> y;
+
+    if(x >= x1 && x <= x2 && y >= y1 && y <= y2){
+        cout << "Inside" << endl;
+    }
+    else
+        cout << "Outside" << endl;
+
+    return 0;
+}
