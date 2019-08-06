108c0b9304e9d36ee5bfe4e44d6d55e837b9b418
Kazalev <kristian.kazalev@abv.bg>
2019-08-06 13:05:19 +0300

    Task

---
+++ Programming Basics with C++ - September 2018/05. Lesson - (while)/Exercises/01. Greatest Common Divisor (CGD)/Greatest Common Divisor (CGD).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a, b, c;
+    cin >> a >> b;
+
+    while(b != 0){
+        c = b;
+        b = a % b;
+        a = c;
+    }
+
+    cout << a << endl;
+
+    return 0;
+}
