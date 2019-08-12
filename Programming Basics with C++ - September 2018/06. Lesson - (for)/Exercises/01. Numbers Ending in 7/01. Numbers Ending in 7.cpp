97dca0e52b7749b3afed00c550d4c9f4a5f2cf17
Kazalev <kristian.kazalev@abv.bg>
2019-08-12 19:01:18 +0300

    Task

---
+++ Programming Basics with C++ - September 2018/06. Lesson - (for)/Exercises/01. Numbers Ending in 7/01. Numbers Ending in 7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    for(int i = 1; i <= 1000; i++){
+
+        if(i % 10 == 7){
+        cout << i << endl;
+        }
+    }
+
+    return 0;
+}
